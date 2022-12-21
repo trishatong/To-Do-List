21102390252158cf8ac6eb65c1e100af2ba22860
ryansevidal <89810484+ryansevidal@users.noreply.github.com>
2022-12-20 17:26:05 -0800

    Server work

--- Server.java
+++ Server.java
@@ -2,15 +2,29 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    public static void main(String[] args) {
+    private static final int PORT_NUM = 2025;
+    private static UserData userData;
+    public Server() {
+    }
+
+    public static void main(String[] var0) {
+        Socket var1 = null;
+        ServerSocket var2 = null;
+
         try {
-            ServerSocket serverSocket = new ServerSocket(6969);
-            Socket socket = serverSocket.accept();
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter pw = new PrintWriter(socket.getOutputStream());
-
+            var2 = new ServerSocket(PORT_NUM);
         } catch (IOException e) {
-
+            e.printStackTrace();
+            System.out.println("Server error");
         }
+    }
+
+    static {
+        FileIO.setFileName("discussionBoard.ser");
+        userData = FileIO.readUserData();
+        if (userData == null) {
+            userData = new UserData();
+        }
+
     }
 }
--- User.java
+++ User.java
@@ -35,4 +35,5 @@ public class User implements Serializable {
     public String getPassword() {
         return password;
     }
+
 }
